c9c3ab6dc0bd4d119e464d92f33d70baadc8dcb6
Kan-Ru Chen <kanru@kanru.info>
2015-12-18 19:54:30 +0800

    Add build command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
+extern crate docopt;
+extern crate rustc_serialize;
+
 pub mod config;
 
+use docopt::Docopt;
+
+#[cfg_attr(rustfmt,rustfmt_skip)]
+const USAGE: &'static str = "
+Usage: memoir build
+       memoir -h
+
+Options:
+    -h, --help    Print this help message.
+";
+
+#[derive(RustcDecodable)]
+struct Args {
+    cmd_build: bool,
+}
+
 fn main() {
-    let config = config::Config::new();
-    println!("{:?}", config);
+    let args: Args = Docopt::new(USAGE)
+                         .and_then(|dopt| dopt.decode())
+                         .unwrap_or_else(|e| e.exit());
+
+    if args.cmd_build {
+        let config = config::Config::new();
+        println!("{:?}", config);
+    }
 }
